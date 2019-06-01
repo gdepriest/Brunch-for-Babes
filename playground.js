dc8196375bf96ea9de40a12d972e8719e3e02d22
Genevieve DePriest <gdpparton@gmail.com>
2019-06-01 10:30:34 -0600

    ajax pull, playground.js file created

---
+++ playground.js
@@ -0,0 +1,13 @@
+$.ajax({
+    url: "https://developers.zomato.com/api/v2.1/search?q=brunch&lat=39.7392&lon=-104.9903&apikey=527733933cfc51b0f78491172626a1a3",
+    method: "POST",
+}).then(function(response) {
+    console.log(response);
+  });
+
+
+
+
+
+
+//   527733933cfc51b0f78491172626a1a3
